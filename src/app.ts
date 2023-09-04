b6575654b592fb9dc23d4830e40b01d2ea8e1815
coderhammer <122128585+coderhammer@users.noreply.github.com>
2023-09-04 11:12:57 +0000

    dx: prettier

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { createServer } from "node:http";
 import { App, createNodeMiddleware } from "octokit";
 import { configuration } from "./utils/configuration.js";
 
-// On créé une nouvelle instance d'application 
+// On créé une nouvelle instance d'application
 // grâce au "SDK" fourni par Github.
 const app = new App(configuration);
 
@@ -13,26 +13,25 @@ const app = new App(configuration);
 //.    scopé sur l'événement que l'on a reçu
 //  2. Payload qui contient un objet représentant l'événement.
 app.webhooks.on("issues.opened", async ({ octokit, payload }) => {
+  console.log("event received!", payload.action);
 
-    console.log('event received!', payload.action)
-    
-    // Nos variables sont "typées" par 
-    // le sdk Octokit fourni par 
-    // Github
-    const repository = payload.repository
-    const issue = payload.issue
+  // Nos variables sont "typées" par
+  // le sdk Octokit fourni par
+  // Github
+  const repository = payload.repository;
+  const issue = payload.issue;
 
-    // Si l'issue n'a aucun "label", 
-    // on ajoute le label "needs triage"
-    if (!issue.labels?.length) {
-      await octokit.rest.issues.addLabels({
-        owner: repository.owner.login,
-        repo: repository.name,
-        issue_number: issue.number,
-        labels: ['needs triage']
-      })
-    }
-    return 'ok'
+  // Si l'issue n'a aucun "label",
+  // on ajoute le label "needs triage"
+  if (!issue.labels?.length) {
+    await octokit.rest.issues.addLabels({
+      owner: repository.owner.login,
+      repo: repository.name,
+      issue_number: issue.number,
+      labels: ["needs triage"],
+    });
+  }
+  return "ok";
 });
 
 // On fait "écouter" notre application sur le port 3000.
@@ -40,4 +39,4 @@ app.webhooks.on("issues.opened", async ({ octokit, payload }) => {
 // qui permettra d'irriguer les différentes fonctions en événement.
 // Exactement comme dans l'exemple qu'on a plus haut sur un
 // événement qui écoute une "opened issue".
-createServer(createNodeMiddleware(app)).listen(process.env.PORT ?? 3000)
+createServer(createNodeMiddleware(app)).listen(process.env.PORT ?? 3000);
